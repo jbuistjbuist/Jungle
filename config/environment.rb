2425b344b160e101d5c0676eb6a1fc7ebd97cd42
Jeremy Buist <79812985+jbuistjbuist@users.noreply.github.com>
2022-11-16 18:44:26 -0500

    Add basic authentication for admin pages

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require_relative "application"
 
+dev_auth = File.join(Rails.root, 'config', 'dev_auth.rb')
+load(dev_auth) if File.exist?(dev_auth)
+
 # Initialize the Rails application.
 Rails.application.initialize!
