c41de07f620da1a8c934d9e9e211fd50a7fedab5
Jeremy Buist <79812985+jbuistjbuist@users.noreply.github.com>
2022-11-16 19:15:43 -0500

    Add summary details to admin dashboard page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,9 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['USER_NAME'], password: ENV['PASS_WORD']
 
   def show
+    @product_count = Product.count 
+    @categories_count = Category.count 
   end
 end
+
+
