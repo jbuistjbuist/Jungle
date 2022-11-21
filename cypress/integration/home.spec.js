807cedbc24b5447d7f50ba18d178f6b293aaedba
Jeremy Buist <79812985+jbuistjbuist@users.noreply.github.com>
2022-11-21 17:33:54 -0500

    Adding first cypress test for home page

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,16 @@
+
+describe("Home Page", () => {
+
+  it ("visits the home page of the site", () => {
+      cy.visit('/')
+  })
+
+  it ("should display products on the page", () => {
+    cy.get(".products article").should("be.visible")
+  })
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+    
+})
